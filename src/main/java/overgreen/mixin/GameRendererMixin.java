750f71c507e782f20ddcb587ae1d7c5e802b90d5
FN <firenamefn@gmail.com>
2026-03-11 06:23:18 +0700

    Reduce bob view while zooming

--- src/main/java/overgreen/mixin/GameRendererMixin.java
+++ src/main/java/overgreen/mixin/GameRendererMixin.java
@@ -5,6 +5,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 
 import net.minecraft.client.DeltaTracker;
@@ -21,5 +22,10 @@ abstract class GameRendererMixin {
     @ModifyReturnValue(method = "getFov", at = @At(value = "RETURN", ordinal = 1), allow = 1)
     private float performZoom(float fov) {
         return fov * (float)OverGreen.getDynamicZoom();
+    }
+
+    @ModifyExpressionValue(method = "bobView", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/entity/ClientAvatarState;getInterpolatedBob(F)F"), allow = 1)
+    private float reduceBobView(float bob) {
+        return bob * (float)OverGreen.getDynamicZoom();
     }
 }
